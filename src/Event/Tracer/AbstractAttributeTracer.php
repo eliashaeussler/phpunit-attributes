78d7b021ed4fe382144ebdaa367839d1ce67ff07
Elias Häußler <elias@haeussler.dev>
2026-01-25 14:02:50 +0100

    [TASK] Run PHP-CS-Fixer

--- src/Event/Tracer/AbstractAttributeTracer.php
+++ src/Event/Tracer/AbstractAttributeTracer.php
@@ -58,7 +58,7 @@ abstract class AbstractAttributeTracer implements Event\Tracer\Tracer
             return;
         }
 
-        if (!($event instanceof Event\Test\Prepared)) {
+        if (!$event instanceof Event\Test\Prepared) {
             return;
         }
 
